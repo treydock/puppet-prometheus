7da9de6c7434df4c981b2a552392be1e4dc4a715
Trey Dockendorf <tdockendorf@osc.edu>
2021-06-24 13:25:13 -0400

    Fix IPMI exporter sudo config, add dependency on saz/sudo

--- spec/acceptance/ipmi_exporter_spec.rb
+++ spec/acceptance/ipmi_exporter_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper_acceptance'
 
 describe 'prometheus ipmi exporter' do
   it 'ipmi_exporter works idempotently with no errors' do
-    # Debian does not automatically have /etc/sudoers.d during tests
-    if fact('os.family') == 'Debian'
-      on hosts, 'mkdir -p /etc/sudoers.d'
-    end
+    on hosts, puppet('module', 'install', 'saz-sudo', '--version', '6.0.0')
     pp = 'include prometheus::ipmi_exporter'
     apply_manifest(pp, catch_failures: true)
     apply_manifest(pp, catch_changes: true)
